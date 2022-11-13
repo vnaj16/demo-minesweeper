5255657ab7b6fb57c71d4f1b5d84e53eb172feeb
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-11-12 21:34:55 -0500

    Finish logic to get the number of surrounded cell mines

--- cell.py
+++ cell.py
@@ -35,9 +35,9 @@ class Cell:
             if cell.x == x and cell.y == y:
                 return cell
 
-    def show_cell(self):
-        self.cell_btn_object.configure(bg='green')
-        surrounded_cells = [
+    @property
+    def surrounded_cells(self):
+        cells = [
             self.get_cell_by_axis(self.x - 1, self.y - 1),
             self.get_cell_by_axis(self.x - 1, self.y),
             self.get_cell_by_axis(self.x - 1, self.y + 1),
@@ -47,7 +47,21 @@ class Cell:
             self.get_cell_by_axis(self.x + 1, self.y + 1),
             self.get_cell_by_axis(self.x, self.y + 1),
         ]
-        print(surrounded_cells)
+        cells = [cell for cell in cells if cell is not None]
+        
+        return cells
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells:
+            if cell.is_mine:
+                counter += 1
+
+        return counter
+
+    def show_cell(self):
+        self.cell_btn_object.configure(bg='green', text=self.surrounded_cells_mines_length)
 
     def show_mine(self):
         # A logic do interrupt the game and display a message that player lost!
