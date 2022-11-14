90f56e0d09d6e1e040f1ce060ff54bdee900caa6
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-11-13 21:09:09 -0500

    Finish display game data logic

--- cell.py
+++ cell.py
@@ -1,14 +1,17 @@
 import random
-from tkinter import Button
+from tkinter import Button, Label
 import settings
 
 class Cell:
     all = []
+    cell_count = settings.CELL_COUNT
+    cell_count_label_object = None
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
         self.cell_btn_object = None
         self.x = x
         self.y = y
+        self.is_opened = False
 
         #Append the object to the Cell.all list - all is a class attribute
         Cell.all.append(self)
@@ -19,16 +22,30 @@ class Cell:
             width=12,
             height=4
         )
-        btn.bind('<Button-1>', self.left_click_actions) #<Button-1> convention de Left click y 3 es Right click
-        btn.bind('<Button-3>', self.right_click_actions) #<Button-1> convention de Left click y 3 es Right click
+        btn.bind('<Button-1>', self.left_click_actions) #<Button-1> Left click
+        btn.bind('<Button-3>', self.right_click_actions) #<Button-3> Right click
         self.cell_btn_object = btn
-        
+    
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg='black',
+            fg='white',
+            text=f"Cells Left: {Cell.cell_count}",
+            font=("", 30)
+        )
+        Cell.cell_count_label_object = lbl
+
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
             self.show_cell()
-
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
+            
     def get_cell_by_axis(self, x, y):
         # Return a cell object based on the value of x,y
         for cell in Cell.all:
@@ -61,7 +78,16 @@ class Cell:
         return counter
 
     def show_cell(self):
-        self.cell_btn_object.configure(bg='green', text=self.surrounded_cells_mines_length)
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+            #Replace the text of the cell count label with newer count
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left: {Cell.cell_count}"
+                )
+        
+        self.is_opened = True
 
     def show_mine(self):
         # A logic do interrupt the game and display a message that player lost!
